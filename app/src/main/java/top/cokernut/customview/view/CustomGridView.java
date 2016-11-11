aa4d1a0b5a0707afcf2a506f30ddb780e4505a19
Cokernut <981100707@qq.com>
2016-11-11 09:43:04 +0800

    修改注释

--- app/src/main/java/top/cokernut/customview/view/CustomGridView.java
+++ app/src/main/java/top/cokernut/customview/view/CustomGridView.java
@@ -11,35 +11,20 @@ import android.widget.GridView;
 public class CustomGridView extends GridView {
 	 
     public CustomGridView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle); 
-        // TODO Auto-generated constructor stub 
-    } 
-    public CustomGridView(Context context, AttributeSet attrs) {
-        super(context, attrs); 
-        // TODO Auto-generated constructor stub 
-    } 
-    public CustomGridView(Context context) {
-        super(context); 
-        // TODO Auto-generated constructor stub 
-    } 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) { 
-        // TODO Auto-generated method stub 
-        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
-        super.onMeasure(widthMeasureSpec, expandSpec);  
- 
-    } 
-    
-    
-    //下面的两个函数注释掉了，使用默认的就好，不然获取不到这个Gridview的Item
-//    @Override
-//    public boolean onInterceptTouchEvent(MotionEvent ev) {
-//            return true;// true 拦截事件自己处理，禁止向下传递
-//    }
-//
-//    @Override
-//    public boolean onTouchEvent(MotionEvent ev) {
-//            return false;// false 自己不处理此次事件以及后续的事件，那么向上传递给外层view
-//    }
+        super(context, attrs, defStyle);
+    }
 
+    public CustomGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomGridView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
 } 
--- app/src/main/java/top/cokernut/customview/view/CustomListView.java
+++ app/src/main/java/top/cokernut/customview/view/CustomListView.java
@@ -24,9 +24,7 @@ public class CustomListView extends ListView {
 
 	@Override
 	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
-		MeasureSpec.AT_MOST);
+		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
 		super.onMeasure(widthMeasureSpec, expandSpec);
 	}
-
 }
